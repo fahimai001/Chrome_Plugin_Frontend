96a664d28a94cd8197706ec6835ebcb2434d50cc
Fahim Sabir <fahimsabircstec001@gmail.com>
2025-01-29 22:48:06 -0800

    Adding api for fetching youtube comments

--- popup.js
+++ popup.js
@@ -3,7 +3,7 @@
 document.addEventListener("DOMContentLoaded", async () => {
     const outputDiv = document.getElementById("output");
     const API_KEY = 'YOUR_API_KEY';  // Replace with your actual YouTube Data API key
-    const API_URL = 'AIzaSyANO8bR2RbmKIB5zMLgURx14-iKDaNUjhc';
+    const API_URL = 'http://my-elb-2062136355.us-east-1.elb.amazonaws.com:80';
   
     // Get the current tab's URL
     chrome.tabs.query({ active: true, currentWindow: true }, async (tabs) => {
